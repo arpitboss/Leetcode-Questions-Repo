6cdd055e7011341539ffb258a6c569954c504153
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-04-22 03:31:23 +0530

    Time: 4 ms (59.13%) | Memory: 8.2 MB (95.67%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,30 +3,27 @@ public:
     string reverseWords(string s) {
         s.erase(s.find_last_not_of(" ") + 1);
         s.erase(0, s.find_first_not_of(" "));
-        vector<string> v;
-        string a="";
-        bool flag=false;
+        reverse(s.begin(),s.end());
+        int c=0;
+        bool flag=true;
         for (int i=0;i<s.size();i++) {
-            if (s[i]==' ') {
-                if (flag==false) {
-                    v.push_back(a);
-                    a="";
-                    flag=true;
-                }
+            if (s[i]!=' ') {
+                c++;
+                flag=true;
             }
             else {
-                a+=s[i];
-                flag=false;
+                if (flag==false) {
+                    s.erase(i,1);
+                    i--;
+                }
+                else {
+                    reverse(s.begin()+(i-c),s.begin()+i);
+                    flag=false;
+                    c=0;
+                }
             }
         }
-        v.push_back(a);
-        
-        string res="";
-        for (int i=v.size()-1;i>=0;i--) {
-            res+=v[i];
-            if (i>0)
-            res+=" ";
-        }
-        return res;
+        reverse(s.begin()+(s.size()-c),s.end());
+        return s;
     }
 };
