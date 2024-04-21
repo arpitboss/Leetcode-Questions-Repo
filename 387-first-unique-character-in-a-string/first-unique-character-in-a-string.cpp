3ddd9bc2df0a4e95c7e249585017f226857791bb
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-04-22 01:21:36 +0530

    Time: 25 ms (72.72%) | Memory: 12.7 MB (91.09%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    const int CHAR = 256;
     int firstUniqChar(string s) {
-       vector<int> v(26,0);
-       
+       int fIndex[CHAR];
+       int res=INT_MAX;
+       fill(fIndex,fIndex+CHAR,-1);
+
        for (int i=0;i<s.size();i++) {
-            v[s[i]-97]++;
+        if (fIndex[s[i]]==-1) {
+            fIndex[s[i]]=i;
+        }
+        else {
+            fIndex[s[i]]=-2;
+        }
        }
 
-       int k=0;
-       for (int i=0;i<s.size();i++) {
-            if (v[s[i]-97]==1) {
-                return i;
-            }
+       for (int i=0;i<CHAR;i++) {
+        if (fIndex[i]>=0) {
+            res=min(res,fIndex[i]);
+        }
        }
-       return -1;
+       return (res==INT_MAX) ? -1 : res;
     }
 };
