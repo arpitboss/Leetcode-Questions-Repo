4ed5b3ed7de1943d2e766688692ecb87c5535a09
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-04-26 16:36:56 +0530

    Time: 14 ms (47.61%) | Memory: 10 MB (51.15%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-
-    bool isAnagram(string &t, string &p, int i) {
-        int count[256]={0};
-        for (int j=0;j<p.size();j++) {
-            count[p[j]]++;
-            count[t[i+j]]--;
-        }
-        for (int j=0;j<256;j++) {
-            if (count[j]!=0) {
-                return false;
-            }
-        }
-        return true;
-    }
     vector<int> findAnagrams(string s, string p) {
         vector<int> v;
-        if (s.size()<p.size()) {
+        int n=s.size();
+        int m=p.size();
+        if (n<m) {
             return v;
         }
-        
-        for (int i=0;i<=s.size()-p.size();i++) {
-            if (isAnagram(s,p,i)) {
-                v.push_back(i);
+        int countT[256];
+        int countP[256];
+        for (int i=0;i<m;i++) {
+            countT[s[i]]++;
+            countP[p[i]]++;
+        }
+
+        for (int i=m;i<=n;i++) {
+            bool flag=true;
+            for (int j=0;j<256;j++) {
+                if (countT[j]!=countP[j]) {
+                    flag=false;
+                }
             }
+            if (flag) {
+                v.push_back(i-m);
+            }
+            countT[s[i]]++;
+            countT[s[i-m]]--;
         }
         return v;
     }
