21d89e40f2f7cea002ff5b2909deb6f7cbe96bfc
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-05-30 15:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (91.68%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> v;
-        map<int,int> mp;
-        for (int i=0;i<nums.size();i++) {
-            mp[nums[i]]++;
-        }
-        for (auto i:mp) {
-            if (i.second>(nums.size()/3)) {
-                v.push_back(i.first);
+        int num1 = INT_MIN, num2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            else if(num2 == element){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = element;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = element;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
-        return v;
+        vector<int> output;
+        int countMajority = nums.size()/3;
+        count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            if(num2 == element){
+                count2++;
+            }
+        }
+        if(count1 > countMajority){
+            output.push_back(num1);
+        }
+        if(count2 > countMajority){
+            output.push_back(num2);
+        }
+        return output;
     }
 };
