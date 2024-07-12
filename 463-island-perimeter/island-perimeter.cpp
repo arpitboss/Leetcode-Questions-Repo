f4d6ff436ca8dd61beda49768bc12521aced0515
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-07-13 04:32:35 +0530

    Time: 74 ms (59.52%) | Memory: 102.2 MB (25.50%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int count=0;
-        for (int i=0;i<grid.size();i++) {
-            for (int j=0;j<grid[0].size();j++) {
-                if (grid[i][j]==1) {
-                    count+=4;
-                    if (i>0 && grid[i-1][j]==1) {
-                        count-=2;
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        int i,j;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<m; j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    if(j==m-1 || (j<m-1 && grid[i][j+1]==0))
+                    {
+                        ans++;
                     }
-                    if (j>0 && grid[i][j-1]==1) {
-                        count-=2;
+                    if(j==0 || (j>0 && grid[i][j-1]==0))
+                    {
+                        ans++;
+                    }
+                    if(i==0 || (i>0 && grid[i-1][j]==0))
+                    {
+                        ans++;
+                    }
+                    if(i==n-1 || (i<n-1 && grid[i+1][j]==0))
+                    {
+                        ans++;
                     }
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
