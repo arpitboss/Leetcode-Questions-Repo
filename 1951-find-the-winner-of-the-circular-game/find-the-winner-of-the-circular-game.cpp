96ef1c74859e3c6953fc96c73927545f6de70ed8
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-07-17 19:29:57 +0530

    Time: 3 ms (61.68%) | Memory: 7.6 MB (34.79%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,47 +1,14 @@
 class Solution {
 public:
-    struct Node {
-        int data;
-        Node *next;
-
-        Node(int x) {
-            data=x;
-            next=NULL;
-        }
-    };
-
-    Node* makeList(int n) {
-      Node *head = new Node(1);
-      Node *curr = head;
-      for (int i=1;i<n;i++) {
-        curr->next= new Node(i+1);
-        curr=curr->next;
-      }
-      curr->next=head;
-      return head;
+    int findTheWinner(int n, int k) {
+       return josephus(n,k)+1;
     }
 
-    int findTheWinner(int n, int k) {
-        Node* head = makeList(n);
-      
-        if (k==1) {
-            return n;
-        }
-    
-        Node* curr = head;
-        while (curr->next != curr) {
-            for (int i=1;i<k-1;i++) {
-                curr = curr->next;
-            }
-            
-            Node* del = curr->next;
-            curr->next = curr->next->next;
-            delete del;
-            curr=curr->next;
-        }
+    int josephus(int n, int k) {
+        if (n==1) {
+           return 0;
+        } 
 
-        int winner = curr->data;
-        delete curr;  
-        return winner;
+        return (josephus(n-1,k)+k)%n;
     }
 };
