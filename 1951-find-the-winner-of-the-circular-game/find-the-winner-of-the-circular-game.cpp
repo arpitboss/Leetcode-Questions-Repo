1c3123329eaa7927531cbca6177629677fb24a5a
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-08-20 20:06:40 +0530

    Time: 9 ms (33.25%) | Memory: 26.9 MB (6.97%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-       return josephus(n,k)+1;
+       vector<int> v;
+       for (int i=1;i<=n;i++) {
+        v.push_back(i);
+       }
+       return josephus(v,k-1,0);
     }
 
-    int josephus(int n, int k) {
-        if (n==1) {
-           return 0;
-        } 
-
-        return (josephus(n-1,k)+k)%n;
+    int josephus(vector<int> v, int k, int index) {
+        if (v.size()==1) {
+            return v[0];
+        }
+        index=(index+k)%v.size();
+        v.erase(v.begin()+index);
+        return josephus(v,k,index);
     }
 };
