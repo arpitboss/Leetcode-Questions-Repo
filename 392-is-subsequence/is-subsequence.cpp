73472b27bcc433661c5bf74b8c92d125217e5385
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-10-13 04:45:16 +0530

    Time: 39 ms (11.17%) | Memory: 288.6 MB (5.21%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
-    string lcs(string s, string t, int n, int m) {
-        vector<vector<int>> dp(n+1, vector<int>(m+1));
-
-        int i,j;
-        for (i=1;i<=n;i++) {
-            for (j=1;j<=m;j++) {
-                if (s[i-1]==t[j-1]) {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+    bool isSubSeq(string str1, string str2, int m, int n) 
+    { 
+        if (m == 0) {
+            return true;
         }
-
-        i=n;
-        j=m;
-        string ans="";
-        while (i>0 && j>0) {
-            if (s[i-1]==t[j-1]) {
-                ans.push_back(s[i-1]);
-                i--;
-                j--;
-            }
-            else {
-                if (dp[i-1][j]>dp[i][j-1]) {
-                    i--;
-                }
-                else {
-                    j--;
-                }
-            }
+        if (n == 0) {
+            return false;
+        } 
+        if (str1[m-1] == str2[n-1]) {
+            return isSubSeq(str1, str2, m-1, n-1);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
+        return isSubSeq(str1, str2, m, n-1); 
+} 
     bool isSubsequence(string s, string t) {
         int n=s.size();
         int m=t.size();
-        return s==lcs(s,t,n,m);
+        return isSubSeq(s,t,n,m);
     }
 };
