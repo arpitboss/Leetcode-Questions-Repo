b929c0449e6c1cded45b181706fd52e757710713
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-11-21 23:02:58 +0530

    Time: 75 ms (19.01%) | Memory: 13.5 MB (19.30%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -11,19 +11,14 @@ public:
             return -1;
         }
 
-        unordered_map<char,int> mp2;
-        mp2['a']=0;
-        mp2['b']=0;
-        mp2['c']=0;
-        
         int res = 0;
         int left = 0;
-        int right=0;
+        int right = 0;
 
         while (right<n) {
-            mp2[s[right]]++;
-            while (mp['a']-mp2['a']<k || mp['b']-mp2['b']<k || mp['c']-mp2['c']<k) {
-                mp2[s[left]]--;
+            mp[s[right]]--;
+            while (mp['a']<k || mp['b']<k || mp['c']<k) {
+                mp[s[left]]++;
                 left++;
             }
             res=max(res,right-left+1);
