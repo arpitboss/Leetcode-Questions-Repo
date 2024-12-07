2cb642ce1a816b9d8af6f97c4eb9739d392b74c0
arpitboss <99241617+arpitboss@users.noreply.github.com>
2024-12-07 17:33:12 +0530

    Time: 106 ms (51.12%) | Memory: 21.7 MB (30.68%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,62 +1,47 @@
 class Solution {
 public:
-    bool diff(string x, string y) {
+    bool diff(const string& x, const string& y) {
         int c=0;
-        for (int i=0;i<x.size();i++) {
+        for (int i = 0; i < x.size(); i++) {
             if (x[i]!=y[i]) {
                 c++;
             }
         }
-        if (c==1) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
+        return (c==1);
     }
+
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> wordSet(wordList.begin(), wordList.end());
         if (wordSet.find(endWord) == wordSet.end()) {
             return 0;
         }
 
-        int res=1;
-        unordered_map<string,vector<string>> graph;
-        vector<string> v;
-        for (auto x:wordList) {
-            if (diff(beginWord,x)==1) {
-                v.push_back(x);
-            }
-        }
-        graph[beginWord]=v;
-
-        for (auto x:wordList) {
-            vector<string> v;
-            for (auto y:wordList) {
-                if (diff(x,y)==1) {
-                    v.push_back(y);
-                }
-            }
-            graph[x]=v;
-        }
-
-        unordered_map<string,int> vis;
         queue<string> q;
         q.push(beginWord);
-        vis[beginWord]=1;
+        int res = 1;
+
+        unordered_map<string, bool> visited;
+        visited[beginWord] = true;
 
         while (!q.empty()) {
-            int n=q.size();
-            for (int i=0;i<n;i++) {
-                string f=q.front();
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                string f = q.front();
                 q.pop();
-                for (auto z:graph[f]) {
-                    if (z==endWord) {
-                        return res+1;
-                    }
-                    if (!vis[z]) {
-                        vis[z]=1;
-                        q.push(z);
+
+                for (int j = 0; j < f.size(); ++j) {
+                    string newWord = f;
+                    for (char ch = 'a'; ch <= 'z'; ++ch) {
+                        newWord[j] = ch;
+
+                        if (newWord == endWord) {
+                            return res + 1;
+                        }
+
+                        if (wordSet.find(newWord) != wordSet.end() && !visited[newWord]) {
+                            visited[newWord] = true;
+                            q.push(newWord);
+                        }
                     }
                 }
             }
