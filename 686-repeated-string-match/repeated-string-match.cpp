2c65413aac4ac8a6f7501aa220961b2f9d4fc842
arpitboss <99241617+arpitboss@users.noreply.github.com>
2025-10-12 16:45:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (42.69%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,20 +1,82 @@
 class Solution {
+private: 
+    void computeLPS(vector<int> &LPS, string &b) {
+        LPS[0]=0;
+        int len=0;
+        int m=b.length();
+        
+        int i=1;
+        while (i<m) {
+            if (b[i]==b[len]) {
+                len++;
+                LPS[i]=len;
+                i++;
+            }
+            else {
+                if (len!=0) {
+                    len=LPS[len-1];
+                }
+                else {
+                    LPS[i]=0;
+                    i++;
+                }
+            }
+        }
+    }
+
+private:
+    int KMP(string &a, string &b) {
+        int n=a.length();
+        int m=b.length();
+
+        vector<int> LPS(m,0);
+        computeLPS(LPS,b);
+
+        int i=0;
+        int j=0;
+        while (i<n) {
+            if (a[i]==b[j]) {
+                i++;
+                j++;
+            }
+            if (j==m) {
+                return i-j;
+            }
+            else if (i<n && a[i]!=b[j]) {
+                if (j!=0) {
+                    j=LPS[j-1];
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+
 public:
     int repeatedStringMatch(string a, string b) {
         int n=a.length();
         int m=b.length();
-        int min_rep=ceil((double)m/n);
+        int lenToAdd=m-1;
 
-        for (int i=0;i<3;i++) {
-            string a_rep="";
-            for (int j=0;j<min_rep+i;j++) {
-                a_rep+=a;
-            }
+        int j=0;
+        for (int i=0;i<lenToAdd;i++) {
+            char ch=a[j];
+            a+=ch;
+            j=(j+1)%n;
+        }
 
-            if (a_rep.find(b)!=string::npos) {
-                return min_rep+i;
+        int stInd=KMP(a,b);
+
+        int res=-1;
+        if (stInd!=-1) {
+            int usedLenOfa=stInd+m;
+            res=usedLenOfa/n;
+            if ((usedLenOfa%n)!=0) {
+                res+=1;
             }
         }
-        return -1;
+        return res;
     }
 };
